ae18b65e77efb489b85d64203ff28e3a6f8d8c81
Tsolo <tsolokhambule@gmail.com>
2025-04-02 10:49:07 +0200

    starting

---
+++ apper/src/main/java/dev/tsolokham/apper/ApperApplication.java
@@ -0,0 +1,13 @@
+package dev.tsolokham.apper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApperApplication.class, args);
+	}
+
+}
---
+++ apper/src/test/java/dev/tsolokham/apper/ApperApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.tsolokham.apper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApperApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
